d44297f87bc877f62ff7cc6aef0eb610b61f2ad3
dchaitanyavarma <dantuluri.chaitanyavarma@tcs.com>
2016-04-19 09:32:18 +0530

    TextField_Label_Button_Label
    TextField_Label_Button_Label Example

---
+++ TextField/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextField
+//
+//  Created by Pavankumar Arepu on 4/19/16.
+//  Copyright © 2016 srk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextField/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextField
+//
+//  Created by Pavankumar Arepu on 4/19/16.
+//  Copyright © 2016 srk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
